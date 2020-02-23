f07de6b6f2ea7fa765ad77fb8ca2b158c08891d0
BruceWu <bruceewue@gmail.com>
2020-02-23 20:32:38 +0800

    increment model version

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -11,7 +11,6 @@ from api import __version__ as api_version
 def test_health_endpoint_returns_200(flask_test_client):
     # When
     response = flask_test_client.get('/health')
-
     # Then
     assert response.status_code == 200
 
